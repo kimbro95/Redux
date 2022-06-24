04f8a29e94d60614c62453ba7e1428102604f990
bro <gudgudgud714@naver.com>
2022-06-24 16:09:27 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import { createStore } from "redux";
 
-const countModifier = (count = 0) => {
-    console.log(count);
-    return count;
+const countModifier = (count = 0, action) => {
+    console.log(count, action);
+    if (action.type === "add") {
+        return count + 1;
+    } else if (action.type === "minus") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "add" });
+countStore.dispatch({ type: "add" });
+countStore.dispatch({ type: "add" });
+countStore.dispatch({ type: "minus" });
+countStore.dispatch({ type: "???" });
+
 console.log(countStore.getState());
