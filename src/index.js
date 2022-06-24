09e25c0f253b56ff34f96f1840419758cb390195
bro <gudgudgud714@naver.com>
2022-06-24 15:42:10 +0900

    Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+import { createStore } from "redux";
 
-let count = 0;
-number.innerText = count;
+const countModifier = (count = 0) => {
+    console.log(count);
+    return count;
+};
 
-const updateText = () => {
-    number.innerText = count;
-}
-const handleAdd = () => {
-    count += 1;
-    updateText();
-}
-
-const handleMinus = () => {
-    count -= 1;
-    updateText();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
