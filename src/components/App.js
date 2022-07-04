17a2109768936650bf726afa38477a29fb6d7bea
bro <gudgudgud714@naver.com>
2022-07-04 16:35:05 +0900

    Setup

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/:id" element={<Detail />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,69 +1,8 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const addToDo = text => {
-    return {
-        type: ADD_TODO,
-        text
-    };
-};
-
-const deleteToDo = id => {
-    return {
-        type: DELETE_TODO,
-        id
-    };
-};
-
-const reducer = (state = [], action) => {
-    switch (action.type) {
-        case ADD_TODO:
-            const newToDoObj = { text: action.text, id: Date.now() };
-            return [newToDoObj, ...state];
-        case DELETE_TODO:
-            const cleaned = state.filter(toDo => toDo.id !== action.id);
-            return cleaned;
-        default:
-            return state;
-    }
-};
-const store = createStore(reducer);
-
-const dispatchAddToDo = text => {
-    store.dispatch(addToDo(text));
-}
-
-const dispatchDeleteToDo = (e) => {
-    const id = parseInt(e.target.parentNode.id);
-    store.dispatch(deleteToDo(id));
-}
-
-const onSubmit = e => {
-    e.preventDefault();
-    const toDo = input.value;
-    input.value = "";
-    dispatchAddToDo(toDo);
-}
-form.addEventListener("submit", onSubmit);
-
-const paintToDos = () => {
-    ul.innerHTML = "";
-    const toDos = store.getState();
-    toDos.forEach(toDo => {
-        const li = document.createElement("li");
-        const btn = document.createElement("button");
-        btn.innerText = "X";
-        btn.addEventListener("click", dispatchDeleteToDo);
-        li.id = toDo.id;
-        li.innerText = toDo.text;
-        li.appendChild(btn);
-        ul.appendChild(li);
-    });
-}
-store.subscribe(paintToDos);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App></App>
+);
