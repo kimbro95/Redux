fd5d85e0c912ac594f4040f1ae6ba1c993f182d6
bro <gudgudgud714@naver.com>
2022-07-14 14:42:11 +0900

    Add Delete Button

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteToDo } from "../store";
 
 function Detail() {
     const id = useParams().id;
     const toDos = useSelector(state => state);
     const toDo = toDos.find(toDo => toDo.id === parseInt(id));
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const handleDeleteToDo = (e) => {
+        e.preventDefault();
+        dispatch(deleteToDo(id));
+        navigate("/");
+    }
+
     return (
         <>
-            <h1>text: {toDo?.text}</h1>
-            <h2>id: {toDo?.id}</h2>
+            <h1>📝 {toDo?.text}</h1>
+            <h2>⏰ {toDo?.id}</h2>
+            <button onClick={handleDeleteToDo}>Delete</button>
         </>
     );
 }
