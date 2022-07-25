436a5bffb7128cf1de09ca384ae1efcb3fc92dc6
bro <gudgudgud714@naver.com>
2022-07-25 13:24:39 +0900

    configureStore

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
-import { createStore } from 'redux';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
 
 export const addToDo = createAction("ADD");
 export const deleteToDo = createAction("DELETE");
@@ -37,6 +36,6 @@ const reducer = createReducer(currentState, {
     }
 });
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export default store;
